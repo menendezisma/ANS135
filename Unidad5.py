05c72623ed80eaa54edf15916ba68ce1526d20f0
dg17001 <dg17001@ues.edu.sv>
2021-06-20 10:33:38 -0300

    Actualizando...

--- Unidad5.py
+++ Unidad5.py
@@ -15,72 +15,161 @@ class Un5:
                 euler=int(input("1.Euler hacia adelante\n2.Euler hacia atras\n3.Euler centrado\n4.Euler modificado\nOtro numero para salir "))
                 while euler > 0 and euler < 5:
                     if euler==1:
-                        # Metod Euler mejorado
+                        # Metodo Euler hacia adelante
+                        #Declarammos nuestras variables
                         x, y = symbols("x y")
+                        #Creamos un try/except para controlar cualquier error
                         try:
+                            #SOlicitamos al usuario que ingrese la funcion
                             fn = eval(input("\x1b[0;30m" + "Ingrese la funcion f(x,y): "))
+                            #Mostramos la funcion al usuario para que el verifique si es correcta
                             print(fn)
+                            #Solicitamos los valores iniciales para x e y
                             x0 = float(input("Ingrese el valor incial de x0: "))
                             y0 = float(input("Ingrese el valor incial de y0: "))
-                            # Creamos un bucle para pedir le al usuario el valor de h y que sea valido
 
+                            # Creamos un bucle para pedir le al usuario el valor de h y que sea valido
                             while True:
+                                #Solicitamos que ingrese el tamaño del paso
                                 h = float(input("Ingrese el tamaño de paso h: "))
+                                #Verificamos que h no sea 0 ni negativo
                                 if h <= 0:
+                                    #De no ser correcto el valor le mandamos un mensaje al usuario
                                     print("El tamaño de paso no puede cer cero o negativo")
                                 else:
+                                    #Si el valor es correcto cerramos el bucle y cntinuamos con el codigo
                                     break
-
+                            #Solicitamos al usuario que ingrese el valor de y a calcular
                             xn = float(input("Ingrese el valor a calcular y(xn): "))
+                            #Calculamos las iteraciones a dar
                             itc = round(((xn - x0) / h))
-                            vx = []
-                            vy = []
+                            #Indicamos al usuario que imprimiremos los valores
                             print("--------- Tabla de valores ---------")
+                            #Creamos un bucle para ir calculando los valores
                             for i in range(1, itc + 1):
+                                #Evaluamos la funcion en los puntos iniciales
                                 f=fn.subs([(x,x0),(y,y0)])
+                                #Calculamos el valor de la aprocimacion
                                 yn = y0 + h *f
+                                #Aumentamos el valor de x
                                 x0 = round(x0 + h, 5)
                                 y0 = yn
+                                #Vamos imprimiendo los valores
                                 print("\x1b[1;35m" + 'y(', x0, ')=', yn)
+                            #Cuando finalizamos indicamos al usuario cual es la respuesta
                             print("\nValor aproximado: ", yn)
 
                         except:
+                            #De haber un error le avisamos al usuario
+                            print("\x1b[1;31m" + "La funcion tiene un problema en sus sintaxis\nPor favor ingrese de nuevo la funcion " + "\x1b[1;35m")
+
+                    elif euler==2:
+                        # Metodo Euler hacia atras
+                        #Declaramos nuestras variables
+                        x, y = symbols("x y")
+                        #creamsos un try/except para manejar los errores que puedan surjir
+                        try:
+                            #Solicitamos al usuario que ingrese la funcion
+                            fn = eval(input("\x1b[0;30m" + "Ingrese la funcion f(x,y): "))
+                            #imprimimos para que el usuario vea si es correcta
+                            print(fn)
+                            #solicitamos los valores iniciales de x e y
+                            x0 = float(input("Ingrese el valor incial de x0: "))
+                            y0 = float(input("Ingrese el valor incial de y0: "))
+                            # Creamos un bucle para pedir le al usuario el valor de h y que sea valido
+                            while True:
+                                #Pedimos el valor de h
+                                h = float(input("Ingrese el tamaño de paso h: "))
+                                #Evaluamos si el valor es correcto
+                                if h <= 0:
+                                    #De no serlo le mandamos un mensaje al usuario
+                                    print("El tamaño de paso no puede cer cero o negativo")
+                                else:
+                                    #Si es correcto cerramos el bucle y continuamos con el desarrollo
+                                    break
+                            #Solicitamos el valor a calcular en la funcion
+                            xn = float(input("Ingrese el valor a calcular y(xn): "))
+                            #calculamos la cantidad de iteraciones a dar
+                            itc = round(((xn - x0) / h))
+                            #Indicamos al usuario que imprimiremos los valores
+                            print("--------- Tabla de valores ---------")
+                            #creamos un bucle para ir calculando los valores
+                            for i in range(1, itc + 1):
+                                #Evaluamos los valores iniciales
+                                f1 = fn.subs([(x, x0), (y, y0)])
+                                #Creamos la variable U para calcular el valor de Yi*
+                                u = y0 + h * f1
+                                #Evaluamos X0+h y u en la funcion para encontrar Yi
+                                f2 = fn.subs([(x, x0 + h), (y, u)])
+                                u2 = y0 + h * f2
+                                print(u)
+                                print(x0+h)
+                                print(u2)
+                                # Calculamos el valor de la aprocimacion
+                                yn = y0 + h * (f1 + f2)
+                                x0 = round(x0 + h, 5)
+                                y0 = yn
+                                # Imprimimos todos los valores al usuario
+                                print("\x1b[1;35m" + 'y(', x0, ')=', yn)
+                            # Cuando finalizamos indicamos al usuario cual es la respuesta
+                            print("\nValor aproximado: ", yn)
+
+                        except:
+                            # De haber un error le avisamos al usuario
                             print(
                                 "\x1b[1;31m" + "La funcion tiene un problema en sus sintaxis\nPor favor ingrese de nuevo la funcion " + "\x1b[1;35m")
 
+
                     elif euler==4:
-                        # Metod Euler mejorado
+                        # Metodo Euler mejorado
+                        #Creamos nuestras variables
                         x, y = symbols("x y")
+                        #Hacemos uso de un try/except para manejar posibles errores
                         try:
+                            #Solicitamos al usuario que ingrese la funcion
                             fn = eval(input("\x1b[0;30m" + "Ingrese la funcion f(x,y): "))
+                            #Se la mostramos al usuario para que verifique si es correcta
                             print(fn)
+                            #Solcitamos los valores iniciales de x e y
                             x0 = float(input("Ingrese el valor incial de x0: "))
                             y0 = float(input("Ingrese el valor incial de y0: "))
                             # Creamos un bucle para pedir le al usuario el valor de h y que sea valido
-
                             while True:
+                                #Pedimos que ingrese el valor de h
                                 h = float(input("Ingrese el tamaño de paso h: "))
+                                #verificamos que el valor sea correcto
                                 if h <= 0:
+                                    #De no serlo devolvemos el siguiente mensaje
                                     print("El tamaño de paso no puede cer cero o negativo")
                                 else:
+                                    #si el valor es correcto cerramos el bucle y seguimos con el desarrollo
                                     break
-
+                            #Solicitamos que ingrese el valor a calcular en la funcion
                             xn = float(input("Ingrese el valor a calcular y(xn): "))
+                            #calculamos la cantidad de iteraciones
                             itc = round(((xn - x0) / h))
-                            vx = []
-                            vy = []
+                            #Indicamos que vamos a imprimir los valores
                             print("--------- Tabla de valores ---------")
+                            #creamos un ciclo for para calcular los valores
                             for i in range(1, itc + 1):
+                                #Evaluamos los valores iniciales de x e y
                                 f1 = fn.subs([(x, x0), (y, y0)])
+                                #utilizamos la variable u para calcular yi*
                                 u = y0 + h * f1
+                                #Evaluamos los valores de x0+h y u en la funcion para encontrar yi
                                 f2 = fn.subs([(x, x0 + h), (y, u)])
+                                #Calculamos el valor de la aprocimacion
                                 yn = y0 + h * ((f1 + f2) / 2)
+                                #calculamos el valor redondeaddo de x para la siguiente iteracion
                                 x0 = round(x0 + h, 5)
                                 y0 = yn
+                                #Imprimimos todos los valores al usuario
                                 print("\x1b[1;35m" + 'y(', x0, ')=', yn)
+                            # Cuando finalizamos indicamos al usuario cual es la respuesta
                             print("\nValor aproximado: ", yn)
 
                         except:
+                            # De haber un error le avisamos al usuario
                             print("\x1b[1;31m" + "La funcion tiene un problema en sus sintaxis\nPor favor ingrese de nuevo la funcion " + "\x1b[1;31m")
 
                     else:
@@ -182,11 +271,14 @@ class Un5:
                         yn = y0 + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
                         x0 = round(x0 + h, 5)
                         y0 = yn
+                        # Imprimimos todos los valores al usuario
                         print("\x1b[1;35m"+'y(', x0, ')=', yn)
+                    # Cuando finalizamos indicamos al usuario cual es la respuesta
                     print("\nValor aproximado: ", yn)
                     break
 
                 except:
+                    # De haber un error le avisamos al usuario
                     print("\x1b[1;31m"+"La funcion tiene un problema en sus sintaxis\nPor favor ingrese de nuevo la funcion "+"\x1b[1;31m")
                 break
             else:
